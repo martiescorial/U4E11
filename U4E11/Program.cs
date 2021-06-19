a200410da1d359e20d34d2cbe349df4ba38f8bf2
Martina <martinaescorial9@gmail.com>
2021-06-19 14:31:32 +0200

    Add project files.

---
+++ U4E11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace U4E11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 5;
+            if (x > y)
+            {
+                Console.WriteLine("x es mayor que y");
+            }else
+            {
+                if (y > x) 
+                {
+                    Console.WriteLine("y es mayor que x");
+                }else
+                    Console.WriteLine("x e y son iguales");
+            }
+        }
+    }
+}
